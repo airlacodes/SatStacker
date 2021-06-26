cd5d5c042daa0cb31b405d4dcc1f83102bf69063
hodlbtcbe3 <rosaci8709@moxkid.com>
2021-06-26 16:16:29 +0100

    Initial Commit

---
+++ SatStacker/SatStackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatStackerApp.swift
+//  SatStacker
+//
+//  Created by Jeevan Thandi on 26/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SatStackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
